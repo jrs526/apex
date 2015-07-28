81c44e935f3df62dfb92ae67f14de31548c5d1b2
Mark Burnett <mark.m.burnett@gmail.com>
2015-07-28 06:49:59 -0500

    remove trailing slashes from endpoints

--- apex/app/urls.py
+++ apex/app/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from rest_framework import routers
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'sums', views.SumViewSet, base_name='sum')
 
 urlpatterns = router.urls
